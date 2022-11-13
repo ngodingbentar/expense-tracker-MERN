b2c8949569dc46c2a30ea4a213a6041ec74e2153
ngodingbentar <dewaqintoro@gmail.com>
2022-11-13 22:18:43 +0700

    added: server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config({ path : "./config.env"});
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
+
