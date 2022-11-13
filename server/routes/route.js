f540d044c20bdc2fa59e501aa666db2010b843de
ngodingbentar <dewaqintoro@gmail.com>
2022-11-13 23:29:59 +0700

    updated: controller

--- server/routes/route.js
+++ server/routes/route.js
@@ -7,14 +7,14 @@ routes.route('/api/cek')
     })
 
 routes.route('/api/categories')
-    .get(controller.create_Categories)
-    // .get(controller.get_Categories)
+    .post(controller.create_Categories)
+    .get(controller.get_Categories)
 
-// routes.route('/api/transaction')
-//     .post(controller.create_Transaction)
-//     .get(controller.get_Transaction)
-//     .delete(controller.delete_Transaction)
+routes.route('/api/transaction')
+    .post(controller.create_Transaction)
+    .get(controller.get_Transaction)
+    .delete(controller.delete_Transaction)
 
-// routes.route('/api/labels')
-//     .get(controller.get_Labels)
+routes.route('/api/labels')
+    .get(controller.get_Labels)
 module.exports = routes;
