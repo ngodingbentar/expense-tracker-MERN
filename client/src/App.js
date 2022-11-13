c4dd3642db362989d860695421f5654b6bbc042a
ngodingbentar <dewaqintoro@gmail.com>
2022-11-13 20:03:15 +0700

    initial

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Graph from './components/Graph';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div>
+      wadudu
+    </div>
+  );
+}
+
+export default App;
