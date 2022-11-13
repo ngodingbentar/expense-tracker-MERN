85cfa8033496d61947eb6a9e2369432a961f9f78
ngodingbentar <dewaqintoro@gmail.com>
2022-11-13 22:39:45 +0700

    Added: controller

---
+++ server/controller/controller.js
@@ -0,0 +1,10 @@
+// const model = require('../models/model');
+
+//  post: http://localhost:8080/api/categories
+async function create_Categories(req, res){
+   res.json("create categories");
+}
+
+module.exports = {
+    create_Categories,
+}
