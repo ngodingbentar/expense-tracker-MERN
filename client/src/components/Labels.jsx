1c9e4e81a99aa0dbdd996582503d584fade99db2
ngodingbentar <dewaqintoro@gmail.com>
2022-11-14 20:10:33 +0700

    1.1

--- client/src/components/Labels.jsx
+++ client/src/components/Labels.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import {default as api} from '../store/apiSlice';
+import { getLabels } from '../helper/helper';
 
-export default function Labels() {
-  const data = [
-    {type: 'Food', color: '#f94144', percent: 50},
-    {type: 'Transport', color: '#f3722c', percent: 20},
-  ]
-
+function LabelComponent({ data }){
+  if(!data) return <></>;
   return (
-    <>
-        {data.map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>)}
-    </>
+    <div className="labels flex justify-between">
+      <div className="flex gap-2">
+        <div className='w-2 h-2 rounded py-3' style={{background: data.color ?? '#f9c74f'}}></div>
+        <h3 className='text-md'>{data.type ?? ''}</h3>
+      </div>
+      <h3 className='font-bold'>{Math.round(data.percent) ?? 0}%</h3>
+    </div>
   )
 }
 
-function LabelComponent({ data }){
-    if(!data) return <></>;
-    return (
-        <div className="labels flex justify-between">
-            <div className="flex gap-2">
-                <div className='w-2 h-2 rounded py-3' style={{background: data.color ?? '#f9c74f'}}></div>
-                <h3 className='text-md'>{data.type ?? ''}</h3>
-            </div>
-            <h3 className='font-bold'>{Math.round(data.percent) ?? 0}%</h3>
-        </div>
-    )
+export default function Labels() {
+  const { data, isFetching , isSuccess, isError } = api.useGetLabelsQuery()
+  let Transactions;
+
+  if(isFetching){
+    Transactions = <div>Fetching</div>;
+  }else if(isSuccess){
+    Transactions = getLabels(data, 'type').map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>);
+  }else if(isError){
+    Transactions = <div>Error</div>
+  }
+
+  return <>{Transactions}</>
 }
--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -1,26 +1,44 @@
 import React from 'react'
 import 'boxicons';
+import {default as api} from '../store/apiSlice';
 
-export default function List() {
-
-    const handlerClick = (e) => {
-        if(!e.target.dataset.id) return 0;
-    }
-
+function Transaction({ category, handler }){
+  console.log('category', category)
+  if(!category) return null;
   return (
-    <div className="flex flex-col py-6 gap-3">
-        <h1 className='py-4 font-bold text-xl'>History</h1>
-        <Transaction category={{ name : 'Sallary', color : '#e5e5e5'}} handler={handlerClick} ></Transaction>
+    <div className="item flex bg-gray-50 py-2 rounded-r" style={{ borderRight : `8px solid ${category.color ??  "#e5e5e5"}`}}>
+      <button className='px-3' onClick={handler}><box-icon data-id={category._id ?? ''}  color={category.color ??  "#e5e5e5"} size="15px" name="trash" ></box-icon></button>            
+      <div className='w-full flex justify-between px-2'>
+        <div>{category.name ?? ''}</div>
+        <div>{category.amount} $</div>
+      </div>
     </div>
   )
 }
 
-function Transaction({ category, handler }){
-    if(!category) return null;
-    return (
-        <div className="item flex justify-center bg-gray-50 py-2 rounded-r" style={{ borderRight : `8px solid ${category.color ??  "#e5e5e5"}`}}>
-            <button className='px-3' onClick={handler}><box-icon data-id={category._id ?? ''}  color={category.color ??  "#e5e5e5"} size="15px" name="trash" ></box-icon></button>            
-            <span className='block w-full'>{category.name ?? ''}</span>
-        </div>
-    )
+export default function List() {
+  const { data, isFetching , isSuccess, isError } = api.useGetLabelsQuery()
+  const [deleteTransaction] = api.useDeleteTransactionMutation()
+  let Transactions;
+
+  
+  const handlerClick = (e) => {
+    if(!e.target.dataset.id) return 0;
+    deleteTransaction({ _id : e.target.dataset.id })
+  }
+
+  if(isFetching){
+    Transactions = <div>Fetching</div>;
+  }else if(isSuccess){
+    Transactions = data.map((v, i) => <Transaction key={i} category={v} handler={handlerClick} ></Transaction>);
+  }else if(isError){
+    Transactions = <div>Error</div>
+  }
+
+  return (
+    <div className="flex flex-col py-6 gap-3">
+      <h1 className='py-4 font-bold text-xl'>History</h1>
+      {Transactions}
+    </div>
+  )
 }
