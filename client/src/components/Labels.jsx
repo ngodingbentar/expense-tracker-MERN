5a3047a3ca1d709007bc74981b1dc977f95152f9
ngodingbentar <dewaqintoro@gmail.com>
2022-11-14 16:32:46 +0700

    updated: get data from DB

--- client/src/components/Labels.jsx
+++ client/src/components/Labels.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { default as api } from '../store/apiSlice'
 
-export default function Labels() {
-  const data = [
-    {type: 'Food', color: '#f94144', percent: 50},
-    {type: 'Transport', color: '#f3722c', percent: 20},
-  ]
-
-  console.log('get cat', api.useGetCategoriesQuery())
-
+function LabelComponent({ data }){
+  if(!data) return <></>;
   return (
-    <>
-        {data.map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>)}
-    </>
+      <div className="labels flex justify-between">
+          <div className="flex gap-2">
+              <div className='w-2 h-2 rounded py-3' style={{background: data.color ?? '#f9c74f'}}></div>
+              <h3 className='text-md'>{data.type ?? ''}</h3>
+          </div>
+          <h3 className='font-bold'>{Math.round(data.percent) ?? 0}%</h3>
+      </div>
   )
 }
 
-function LabelComponent({ data }){
-    if(!data) return <></>;
-    return (
-        <div className="labels flex justify-between">
-            <div className="flex gap-2">
-                <div className='w-2 h-2 rounded py-3' style={{background: data.color ?? '#f9c74f'}}></div>
-                <h3 className='text-md'>{data.type ?? ''}</h3>
-            </div>
-            <h3 className='font-bold'>{Math.round(data.percent) ?? 0}%</h3>
-        </div>
-    )
+export default function Labels() {
+
+  const { data, isFetching , isSuccess, isError } = api.useGetLabelsQuery()
+    let Transactions;
+
+  console.log('data', data, 'isFetching', isFetching, 'isSuccess', isSuccess, 'isError', isError)
+
+  if(isFetching){
+      Transactions = <div>Fetching</div>;
+  }else if(isSuccess){
+      Transactions = data.map((v, i) => <LabelComponent key={i} data={v}></LabelComponent>)
+  }else if(isError){
+      Transactions = <div>Error</div>
+  }
+
+  return (
+    <>
+      {Transactions}
+    </>
+  )
 }
